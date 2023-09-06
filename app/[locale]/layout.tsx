cc67e2c3b9cad1941e913341d91f26e2024be069
Santiago Nicolás Grufi <santiago.grufi@wearevernon.com>
2023-09-06 12:21:15 -0300

    fix layout params

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -5,9 +5,15 @@ import { Inter } from 'next/font/google'
 import IntlPolyfills from '../components/IntlPolyfills'
 import {NextIntlClientProvider} from 'next-intl'
 import {notFound} from 'next/navigation'
+import {ReactNode} from "react";
 
 
 const inter = Inter({ subsets: ['latin'] })
+
+type Props = {
+  children: ReactNode;
+  params: {locale: string};
+};
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -18,13 +24,15 @@ export function generateStaticParams() {
   return [{locale: 'en'}, {locale: 'es'}];
 }
 
-export default async function LocaleLayout({children, params: {locale}}) {
+export default async function LocaleLayout({
+    children,
+    params: {locale}
+  }: Props) {  let messages
 
-  let messages;
   try {
     messages = (await import(`../../messages/${locale}.json` as string)).default;
   } catch (error) {
-    notFound();
+    notFound()
   }
 
   return (
